01e03b302550d2afce9f66872c16feb587efb76f
HBChawla <deepakchawla35@gmail.com>
2017-04-15 01:05:53 +0530

    simple commit commit it

--- index.php
+++ index.php
@@ -20,16 +20,45 @@ $userID = $input['entry'][0]['messaging'][0]['sender']['id'];
 
 $message =  $input['entry'][0]['messaging'][0]['message']['text'];
 
-$reply = "I don't understand. Ask me to 'tell a joke'";
+#$reply = "I don't understand. Ask me to 'tell a joke'";
 
-if(preg_match('/(send|tell|text)(.*?)joke/',$message)){
+/*if(preg_match('/(send|tell|text)(.*?)joke/',$message)){
   $res = json_decode(file_get_contents('http://api.yomomma.info/'),true);
   $reply = $res['joke'];
+}*/
+switch ($message) {
+  case (preg_match('/(Hello|hello|Hy|hy|Hi|hi|Hey|hey)/',$message) ? true :false ):
+    $reply = "Hello Mr. APS";
+    break;
+    case (preg_match('/(How are you|how are you)/',$message) ? true :false ):
+      $reply = "I am fit as fiddle.... How are you...??";
+      break;
+    case (preg_match('/(I am fine|I am good|i am fine|i am good)/',$message) ? true :false ):
+      $reply = "It is very good";
+      break;
+  default:
+    $reply = "I don't understand. Ask me to 'tell a joke'";
+    break;
 }
-
 
 $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$access_token";
 
+
+
+/*$jsonData = "{
+  'recipient' :{
+  'id' : $userID
+  },
+  'message':{
+    'attachment':{
+      'type':'image',
+      'payload':{
+        'url' : 'https://b7bc5329.ngrok.io/hbchawla_jarvis/deepakchawla.jpg'
+      }
+    }
+  }
+}";
+*/
 
 $jsonData = "{
   'recipient' :{
