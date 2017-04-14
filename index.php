3620839ca11e3ab9313e840f728543c0f98d0328
HBChawla <deepakchawla35@gmail.com>
2017-04-15 01:12:29 +0530

    simple commit commit it

--- index.php
+++ index.php
@@ -27,18 +27,30 @@ $message =  $input['entry'][0]['messaging'][0]['message']['text'];
   $reply = $res['joke'];
 }*/
 switch ($message) {
-  case (preg_match('/(Hello|hello|Hy|hy|Hi|hi|Hey|hey)/',$message) ? true :false ):
-    $reply = "Hello Mr. APS";
-    break;
+    case (preg_match('/(Hello|hello|Hy|hy|Hi|hi|Hey|hey)/',$message) ? true :false ):
+      $reply = "Hello Mr. APS";
+      break;
     case (preg_match('/(How are you|how are you)/',$message) ? true :false ):
       $reply = "I am fit as fiddle.... How are you...??";
       break;
     case (preg_match('/(I am fine|I am good|i am fine|i am good)/',$message) ? true :false ):
       $reply = "It is very good";
       break;
-  default:
-    $reply = "I don't understand. Ask me to 'tell a joke'";
-    break;
+    case (preg_match('/(Who are you|What is your name|who are you|what is your name)/',$message) ? true :false ):
+      $reply = "My name is HBChawla Jarvis and I am an AI based personal home assistant..";
+      break;
+    case (preg_match('/(Who develop you|who develop you|/',$message) ? true :false ):
+      $reply = "Mr. Deepak Chawla. He is a MLVTEC Final Year Engineering Student";
+      break;
+    case (preg_match('/(like|Like|/',$message) ? true :false ):
+      $reply = "I like too";
+      break;
+    case (preg_match('/(love|Love|/',$message) ? true :false ):
+      $reply = "I love too";
+      break;
+    default:
+      $reply = "Sorry, I don't understand.";
+      break;
 }
 
 $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$access_token";
